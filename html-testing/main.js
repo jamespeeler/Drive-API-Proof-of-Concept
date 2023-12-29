8502369812f53623ed28677faf5c46989f2af7cb
jamespeeler <james.peeler.a@gmail.com>
2023-12-28 22:54:26 -0500

    add main.js - site now fills in current address and address of delivering store based on select elements

---
+++ html-testing/main.js
@@ -0,0 +1,81 @@
+let currentLocationSelect = document.getElementById('currentLocationSelect')
+let deliveringLocationSelect = document.getElementById('deliveringLocationSelect')
+
+currentLocationSelect.addEventListener('change', insertCurrentAddress)
+deliveringLocationSelect.addEventListener('change', insertDeliveringAddress)
+
+let currentStreet = document.querySelector('.currentStreet')
+let currentCity = document.querySelector('.currentCityName')
+let currentZip = document.querySelector('.currentZipCode')
+
+let deliveringStreet = document.querySelector('.deliveringStreet')
+let deliveringCity = document.querySelector('.deliveringCityName')
+let deliveringZip = document.querySelector('.deliveringZipCode')
+
+let addresses = {
+    branch1: {
+        street: "123 Example Street",
+        cityName: "Kissimmee, FL",
+        zipCode: "34746"
+    },
+    branch2: {
+        street: "345 Sample Road",
+        cityName: "Poinciana, FL",
+        zipCode: "33837"
+    },
+    branch3: {
+        street: "678 Model Blvd",
+        cityName: "Orlando, FL",
+        zipCode: "32789"
+    }
+}
+
+currentStreet.innerHTML = addresses.branch1.street
+currentCity.innerHTML = addresses.branch1.cityName
+currentZip.innerHTML = addresses.branch1.zipCode
+
+deliveringStreet.innerHTML = addresses.branch1.street
+deliveringCity.innerHTML = addresses.branch1.cityName
+deliveringZip.innerHTML = addresses.branch1.zipCode
+
+
+
+function insertCurrentAddress(){
+   
+    let currentSelection =  currentLocationSelect.value
+
+    if (currentSelection === 'currentBranch1'){
+        currentStreet.innerHTML = addresses.branch1.street
+        currentCity.innerHTML = addresses.branch1.cityName
+        currentZip.innerHTML = addresses.branch1.zipCode
+    } else if (currentSelection === 'currentBranch2'){
+        currentStreet.innerHTML = addresses.branch2.street
+        currentCity.innerHTML = addresses.branch2.cityName
+        currentZip.innerHTML = addresses.branch2.zipCode
+    } else if (currentSelection === 'currentBranch3'){
+        currentStreet.innerHTML = addresses.branch3.street
+        currentCity.innerHTML = addresses.branch3.cityName
+        currentZip.innerHTML = addresses.branch3.zipCode
+    }
+
+    
+}
+
+function insertDeliveringAddress(){
+
+    let deliveringSelection = deliveringLocationSelect.value
+    
+    if (deliveringSelection === 'deliveringBranch1'){
+        deliveringStreet.innerHTML = addresses.branch1.street
+        deliveringCity.innerHTML = addresses.branch1.cityName
+        deliveringZip.innerHTML = addresses.branch1.zipCode
+    } else if (deliveringSelection === 'deliveringBranch2'){
+        deliveringStreet.innerHTML = addresses.branch2.street
+        deliveringCity.innerHTML = addresses.branch2.cityName
+        deliveringZip.innerHTML = addresses.branch2.zipCode
+    } else if (deliveringSelection === 'deliveringBranch3'){
+        deliveringStreet.innerHTML = addresses.branch3.street
+        deliveringCity.innerHTML = addresses.branch3.cityName
+        deliveringZip.innerHTML = addresses.branch3.zipCode
+    }
+}
